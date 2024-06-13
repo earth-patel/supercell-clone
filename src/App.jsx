329673d4291f69fd5a65a4ee2dfa20c51b9c7609
Astik Singh <assi@odoo.com>
2024-06-13 09:24:42 +0530

    Add Tittle and image at top right on main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Header from "./components/header/header";
 import "./index.scss";
+import Title from "./components/title/title"
+import spirit from "./assets/images/heal_spirit.png";
 
 function App() {
   return (
     <>
       <Header />
+      <Title />
+      <div className="heal-spirit w-96 absolute aspect-square">
+        <img className="" src={spirit} alt="heal_spirit" />
+      </div>
     </>
   );
 }
---
+++ src/components/title/title.jsx
@@ -0,0 +1,13 @@
+import "./title.scss";
+
+const Title = () => {
+    return (
+        <div className="w-full h-auto">
+                <div className="text-full-width font-bold text-center h-auto">
+                    SUPERCELL
+                </div>
+        </div>
+    );
+};
+
+export default Title;
